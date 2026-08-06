091f87669b1823867a0fdf28745852bfa792f718
Valeria Li <vlitang9@gmail.com>
2026-08-06 16:41:40 -0400

    cv

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -16,7 +16,7 @@
 						</svg>
 						<span>LinkedIn</span>
 					</a>
-					<a href="https://drive.google.com/file/d/1-wNRrnvLswu9fyPkHGMWCuY4fWhxraMg/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+					<a href="https://drive.google.com/file/d/10k8HqExS6GvjM_n2VruGgNIWhOj6935a/view?usp=sharing" target="_blank" rel="noopener noreferrer">
 						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 							<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
 							<polyline points="14 2 14 8 20 8"></polyline>
